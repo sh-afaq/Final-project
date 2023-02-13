1366aaeef9b3271e6efea6392e5be22104daedbc
shafaq sabeen <95220716+sh-afaq@users.noreply.github.com>
2023-02-13 22:24:12 +0500

    table entities added

--- src/ITWebsite/BusinessServices/IT.Business.Models/UserModel.cs
+++ src/ITWebsite/BusinessServices/IT.Business.Models/UserModel.cs
@@ -4,5 +4,7 @@
     {
         public int id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
